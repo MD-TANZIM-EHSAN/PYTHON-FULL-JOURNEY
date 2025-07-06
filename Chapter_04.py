c31f74dd26dbb798784413dd4837bfa948c20733
MD-TANZIM-EHSAN <mdtanzime@gmail.com>
2025-07-06 20:52:29 +0600

    Add files via upload

---
+++ Chapter_04.py
@@ -0,0 +1,14 @@
+#String
+# CORE 
+chai_type ="Ginger chai"
+Customer_name = "Priya"
+print(f"Order for {Customer_name} AND the chai type is: {chai_type}" )
+
+Chai_description = "Aromatic and bold "
+print(f"The first word : {Chai_description[0:8]}")
+
+lebel_text = "Chai special"
+encoded_level= lebel_text.encode("utf-8")
+print(encoded_level)
+decode_text = encoded_level.decode("utf-8")
+print(decode_text )
