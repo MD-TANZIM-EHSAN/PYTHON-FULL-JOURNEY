2b07137a4e35fde8eb5b1d9d5028fdfcec2bbda2
MD-TANZIM-EHSAN <mdtanzime@gmail.com>
2025-07-06 20:52:14 +0600

    Add files via upload

---
+++ Chapter_03.py
@@ -0,0 +1,33 @@
+# INTEGER 
+
+
+black_tea_grams = 14
+ginger_grams = 3
+total_grams = black_tea_grams + ginger_grams
+print(total_grams)
+
+remaining_tea = black_tea_grams - ginger_grams
+print(remaining_tea)
+
+milk_litre = 7
+serving =4
+milk_serving_litre = milk_litre / serving
+print(milk_serving_litre)
+
+total_tea_bag =7
+pot = 4   
+bags_per_pot = total_tea_bag // pot
+print(f"bag per pot : {bags_per_pot}")
+
+total_cadamom_pot = 10
+pod_per_cup = 3
+left_over_pods =  total_cadamom_pot % pod_per_cup
+print(left_over_pods)
+
+base_flavour_strenght = 2 
+scale_factor = 3
+powerful_flavour = base_flavour_strenght ** scale_factor
+print(powerful_flavour)
+
+billion = 1_000_000_000
+print(billion)
